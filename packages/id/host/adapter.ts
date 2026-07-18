a786a1fec249d16dbe2dc08fe96fecf496397a21
Drew Youngwerth <drew@youngwerth.com>
2026-07-17 22:17:19 -0700

    Convert ID package to pure Voyd

--- packages/id/host/adapter.ts
+++
@@ -1,9 +0,0 @@
-import { defineAdapter } from "../generated/voyd-adapter.js";
-import { id, wordId } from "../src/index.js";
-
-export default defineAdapter({
-  "tessyl:id/generator@1": {
-    id,
-    word_id: (wordCount) => wordId({ wordCount }),
-  },
-});
